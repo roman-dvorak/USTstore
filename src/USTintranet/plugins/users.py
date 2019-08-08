6133c4d3fda1268774bcfc78efade969f643c542
Tomáš Derner <tomasderner97@gmail.com>
2019-08-08 22:06:34 +0200

    zprovoznena karta uzivatele, zmeny kde vsude

--- src/USTintranet/plugins/users.py
+++ src/USTintranet/plugins/users.py
@@ -6,108 +6,13 @@ import bson.json_util
 
 from . import BaseHandler
 from .users_helpers import database as db
-
-EXPECTED_FIELDS = [
-    "user",
-    "pass",
-    "email",
-    "email_validate",
-    "role",
-    "created",
-    "type",
-    "name",
-    "pre_name_title",
-    "first_name",
-    "surname",
-    "post_name_title",
-    "birthdate",
-    "addresses",
-    "phone_number",
-    "account_number",
-    "assignment",
-    "contracts",
-    "skills",
-    "notes",
-    "work_spans",
-    "vacations",
-    "documents",
-    "wages",
-    "month_closed",
-]
-
-EMPTY_USER_DOC = {
-    "user": None,
-    "pass": None,
-    "email": None,
-    "email_validate": None,
-    "role": [],
-    "created": None,
-    "type": None,
-    "name": None,
-    "birthdate": None,
-    "addresses": [],
-    "phone_number": None,
-    "account_number": None,
-    "assignment": None,
-    "contracts": [],
-    "skills": None,
-    "notes": None,
-    "work_spans": [],
-    "vacations": [],
-    "documents": [],
-    "wages": [],
-    "month_closed": None,
-}
-EMPTY_NAME_DOC = {
-    "pre_name_title": None,
-    "first_name": None,
-    "surname": None,
-    "post_name_title": None,
-}
-EMPTY_ADDRESS_DOC = {
-    "street": None,
-    "city": None,
-    "state": None,
-    "zip": None,
-    "type": None,
-}
-EMPTY_CONTRACT_DOC = {
-    "type": None,
-    "signing_date": None,
-    "valid_from": None,
-    "valid_until": None,
-    "hour_rate": None,
-    "is_valid": None,
-}
-EMPTY_DOCUMENT_DOC = {
-    "type": None,
-    "valid_from": None,
-    "valid_until": None,
-    "path_to_file": None,
-}
-EMPTY_VACATION_DOC = {
-    "from": None,
-    "until": None,
-    "do_not_disturb": None,
-}
-EMPTY_WAGE_DOC = {
-    "month": None,
-    "hours_worked": None,
-    "gross_wage": None,
-    "is_taxed": None,
-    "net_wage": None,
-}
-EMPTY_WORKSPAN_DOC = {
-    "from": None,
-    "hours": None,
-    "note": None,
-    "assignment": None,
-}
+from .users_helpers import tostr
 
 
 def make_handlers(plugin_name, plugin_namespace):
     return [
-        (r'/{}/api/user/admintable'.format(plugin_name), plugin_namespace.ApiAdminTableHandler),
+        (r'/{}/api/admintable'.format(plugin_name), plugin_namespace.ApiAdminTableHandler),
+        (r'/{}/api/u/(.*)/edit'.format(plugin_name), plugin_namespace.ApiEditUserHandler),
         (r'/{}/u/(.*)'.format(plugin_name), plugin_namespace.UserPageHandler),
         (r'/{}'.format(plugin_name), plugin_namespace.HomeHandler),
         (r'/{}/'.format(plugin_name), plugin_namespace.HomeHandler),
@@ -143,10 +48,10 @@ class ApiAdminTableHandler(BaseHandler):
         data = db.get_users(self.mdb.users)
 
         for item in data:
-            item["_id"] = str(item["_id"])
-
             if "created" in item:
                 item["created"] = item["created"].replace(microsecond=0).isoformat()
+            if "birthdate" in item:
+                item["birthdate"] = item["birthdate"].replace(microsecond=0).isoformat()
 
             item.pop("pass", None)
 
@@ -171,7 +76,8 @@ class ApiAdminTableHandler(BaseHandler):
             "deleted": [<id 3>, <další id smazaných uživatelů>],
         }
         """
-        data = bson.json_util.loads(self.request.body.decode("utf-8"))
+        req = self.request.body.decode("utf-8")
+        data = bson.json_util.loads(req)
 
         edited_data = data["edited"]
         new_ids = data["new"]
@@ -180,7 +86,8 @@ class ApiAdminTableHandler(BaseHandler):
         for _id in deleted_ids:
             db.delete_user(self.mdb.users, _id)
 
-        db.add_users(self.mdb.users, new_ids)
+        if new_ids:
+            db.add_users(self.mdb.users, new_ids)
 
         for _id, fields in edited_data.items():
             self.process_and_update(_id, fields)
@@ -193,7 +100,7 @@ class ApiAdminTableHandler(BaseHandler):
         residence_address = {key.split(".")[1]: data.pop(key) for key in keys if "residence_address" in key}
         contact_address = {key.split(".")[1]: data.pop(key) for key in keys if "contact_address" in key}
 
-        if "birthdate" in data:
+        if "birthdate" in data and data["birthdate"] != "":
             data["birthdate"] = datetime.strptime(data["birthdate"], "%Y-%m-%d")
 
         if data:
@@ -208,15 +115,72 @@ class ApiAdminTableHandler(BaseHandler):
             db.update_user_address(self.mdb.users, _id, contact_address)
 
 
+class ApiEditUserHandler(BaseHandler):
+
+    def post(self, _id):
+        req = self.request.body.decode("utf-8")
+        changes = bson.json_util.loads(req)
+
+        residence_address = changes.pop("residence_address", {})
+        contact_address = changes.pop("contact_address", {})
+
+        if "birthdate" in changes and changes["birthdate"] != "":
+            changes["birthdate"] = datetime.strptime(changes["birthdate"], "%Y-%m-%d")
+
+        if changes:
+            db.update_user(self.mdb.users, _id, changes)
+
+        if residence_address:
+            residence_address["type"] = "residence"
+            db.update_user_address(self.mdb.users, _id, residence_address)
+        if contact_address:
+            contact_address["type"] = "contact"
+            db.update_user_address(self.mdb.users, _id, contact_address)
+
+
 class UserPageHandler(BaseHandler):
 
     def get(self, _id):
-        print(_id)
         user_document = db.get_user(self.mdb.users, _id)
-        print(user_document)
 
-        for key in EXPECTED_FIELDS:
-            if key not in user_document:
-                user_document[key] = None
+        name_doc = user_document.get("name", {})
+        res_address_doc = next((a for a in user_document.get("addresses", {}) if a["type"] == "residence"), {})
+        cont_address_doc = next((a for a in user_document.get("addresses", {}) if a["type"] == "contact"), {})
 
-        self.render("users.user-page.hbs", user_doc=user_document)
+        birthdate = user_document.get("birthdate", None)
+
+        template_params = {
+            "user": user_document.get("user", ""),
+            "_id": user_document.get("_id"),
+
+            "name": tostr.name_to_str(name_doc),
+            "pre_name_title": name_doc.get("pre_name_title", ""),
+            "first_name": name_doc.get("first_name", ""),
+            "surname": name_doc.get("surname", ""),
+            "post_name_title": name_doc.get("post_name_title", ""),
+
+            "birthdate": tostr.date_to_str(birthdate),
+            "birthdate_iso": tostr.date_to_iso_string(birthdate),
+
+            "residence_address": tostr.address_to_str(res_address_doc),
+            "residence_street": res_address_doc.get("street", ""),
+            "residence_city": res_address_doc.get("city", ""),
+            "residence_state": res_address_doc.get("state", ""),
+            "residence_zip": res_address_doc.get("zip", ""),
+
+            "contact_address": tostr.address_to_str(cont_address_doc),
+            "contact_street": cont_address_doc.get("street", ""),
+            "contact_city": cont_address_doc.get("city", ""),
+            "contact_state": cont_address_doc.get("state", ""),
+            "contact_zip": cont_address_doc.get("zip", ""),
+
+            "email": user_document.get("email", ""),
+            "phone_number": user_document.get("phone_number", ""),
+            "account_number": user_document.get("account_number", ""),
+            "role": ", ".join(user_document.get("role", [])),
+            "assignment": user_document.get("assignment", ""),
+            "skills": user_document.get("skills", ""),
+            "notes": user_document.get("notes", ""),
+        }
+
+        self.render("users.user-page.hbs", **template_params)
--- src/USTintranet/plugins/users_helpers/database.py
+++ src/USTintranet/plugins/users_helpers/database.py
@@ -5,44 +5,81 @@ import pymongo
 from bson import ObjectId
 import warnings
 
+from pymongo.collection import ReturnDocument
+
 coll = pymongo.MongoClient().USTintranet.users
 
 
 def get_users(coll: pymongo.collection.Collection):
-    users = coll.find({'type': 'user'})
-    return list(users)
+    users = list(coll.find({'type': 'user'}))
+    for user in users:
+        user["_id"] = str(user["_id"])
+    return users
 
 
 def get_user(coll: pymongo.collection.Collection, _id: str):
     user = coll.find_one({'_id': ObjectId(_id)})
+    user["_id"] = str(user["_id"])
     return user
 
 
-def update_user(coll: pymongo.collection.Collection, _id: str, data: dict):
+def update_user(coll: pymongo.collection.Collection, _id: str, data: dict, embedded_1to1_docs=("name",)):
     """
     Updatuje data existujícího uživatele v databázi. Data by měla být first level fieldy a fieldy jednotlivě embedded
     dokumentů v dot notation. Pro updatování polí embedded dokumentů jsou speciální funkce.
+    Je-li hodnota určitého fieldu "" (prázdný řetězec), field je z dokumentu odstraněn (je-li poslední v embedded
+    dokumentu, embedded dokument je odstraněn).
     """
-    coll.update_one({"_id": ObjectId(_id)}, {"$set": data})
+    to_unset = {key: "" for key, value in data.items() if value == ""}
+    for key in to_unset:
+        del data[key]
+
+    operation_dict = {}
+    if data:
+        operation_dict["$set"] = data
+    if to_unset:
+        operation_dict["$unset"] = to_unset
+
+    updated = coll.find_one_and_update({"_id": ObjectId(_id)}, operation_dict, return_document=ReturnDocument.AFTER)
+
+    for key in embedded_1to1_docs:
+        if not updated[key]:
+            coll.update_one({"_id": ObjectId(_id)}, {"$unset": {"name": ""}})
 
 
 def update_user_address(coll: pymongo.collection.Collection, _id: str, address: dict):
     """
     Nastaví pro daného uživatele adresu. Dict address musí obsahovat field "type". Uživatel může mít jen jednu adresu
     daného typu.
+    Je-li hodnota určitého fieldu "" (prázdný řetězec), field je z adresy odstraněn (je-li poslední v adrese kromě
+    "type", je adresa odstraněna).
+
     """
     address_type = address["type"]
-    print("address_type", address_type)
 
+    to_unset = {key: "" for key, value in address.items() if value == ""}
+    for key in to_unset:
+        del address[key]
+
+    # pokud už je tento typ adresy v databázi
     if coll.find_one({"_id": ObjectId(_id), "addresses.type": address_type}):
-        print("updating")
-        update_dict = {f"addresses.$.{key}": value for key, value in address.items()}
 
-        coll.update_one({"_id": ObjectId(_id), "addresses.type": address_type},
-                        {"$set": update_dict})
+        operation_dict = {}
+        if len(address) > 1:  # je tam něco kromě "type"
+            operation_dict["$set"] = {f"addresses.$.{key}": value for key, value in address.items()}
+        if to_unset:
+            operation_dict["$unset"] = {f"addresses.$.{key}": value for key, value in to_unset.items()}
 
-    else:
-        print("new")
+        updated = coll.find_one_and_update({"_id": ObjectId(_id), "addresses.type": address_type}, operation_dict,
+                                           return_document=ReturnDocument.AFTER)
+
+        # smaž adresu z "addresses", pokud po updatu obsahuje pouze "type"
+        for address in updated["addresses"]:
+            if address["type"] == address_type and len(address) <= 1:
+                delete_user_address(coll, _id, address_type)
+
+    # jinak přidej adresu do databáze, pokud obsahuje víc než jen "type"
+    elif len(address) > 1:
         coll.update_one({"_id": ObjectId(_id)},
                         {"$addToSet": {
                             "addresses": address
@@ -54,8 +91,13 @@ def delete_user_address(coll: pymongo.collection.Collection, _id: str, address_t
     """
     Smaže daný typ adresy z databáze.
     """
-    # TODO
-    pass
+    # TODO implementovat odstranění fieldu když je hodnota fieldu prázdná
+    coll.update_one({"_id": ObjectId(_id)},
+                    {"$pull": {
+                        "addresses": {
+                            "type": address_type
+                        }
+                    }})
 
 
 def add_users(coll: pymongo.collection.Collection, ids: list = None, n: int = None):
