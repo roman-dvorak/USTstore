08cbc5f27f0ba44e2003324b085321325457ed1f
Tomáš Derner <tomasderner97@gmail.com>
2019-12-19 00:33:25 +0100

    pocitani dani

--- src/USTintranet/plugins/attendance.py
+++ src/USTintranet/plugins/attendance.py
@@ -13,6 +13,7 @@ from plugins.helpers import database_user as udb
 from plugins.helpers import str_ops
 from plugins.helpers.exceptions import BadInputError
 from plugins.helpers.finance import calculate_tax
+from plugins.helpers.math_utils import floor_to_half
 from plugins.helpers.mdoc_ops import compile_user_month_info, get_user_days_of_vacation_in_year
 
 
@@ -21,6 +22,7 @@ def make_handlers(plugin_name, plugin_namespace):
         (r'/{}/u/(.*)/date/(.*)'.format(plugin_name), plugin_namespace.UserAttendanceHandler),
         (r'/{}/u/(.*)'.format(plugin_name), plugin_namespace.UserAttendanceHandler),
         (r'/{}/api/u/(.*)/workspans'.format(plugin_name), plugin_namespace.ApiAddWorkspanHandler),
+        (r'/{}/api/u/(.*)/workspans/month'.format(plugin_name), plugin_namespace.ApiMonthWorkspansHandler),
         (r'/{}/api/u/(.*)/calendar/date/(.*)'.format(plugin_name), plugin_namespace.ApiCalendarHandler),
         (r'/{}/api/u/(.*)/monthinfo/date/(.*)'.format(plugin_name), plugin_namespace.ApiMonthInfoHandler),
         (r'/{}/api/u/(.*)/workspans/delete'.format(plugin_name), plugin_namespace.ApiDeleteWorkspanHandler),
@@ -72,6 +74,8 @@ class AttendanceCalculator:
         self._month_gross_wage = None
         self._month_tax_amount = None
 
+        self._month_hour_rate = None
+
         self._year_hours_worked = None
         self._year_gross_wage = None
         self._year_tax_amount = None
@@ -84,27 +88,36 @@ class AttendanceCalculator:
         self._month_tax_amount = tax_amount
 
     def _calculate_given_month(self, month: datetime):
+        print("_calculate_given_month", month)
+        if month == self.month and self._month_hours_worked is not None:
+            print("tento měsíc je už spočítaný")
+            return self._month_hours_worked, self._month_gross_wage, self.month_tax_amount
+
         next_month = month + relativedelta(months=1)
 
         workspans = adb.get_user_workspans(self.database, self.user_id, month, next_month)
 
-        has_study_certificate = bool(udb.get_user_active_documents(self.database, self.user_id, "study_certificate",
-                                                                   month, next_month))
-        has_tax_declaration = bool(udb.get_user_active_documents(self.database, self.user_id, "tax_declaration",
-                                                                 month, next_month))
+        has_study_certificate = self._month_has_study_certificate(month)
+        has_tax_declaration = self._month_has_tax_declaration(month)
+
         contract = None
 
         hours_worked = 0
-        gross_wage = 0
         for workspan in workspans:
-            if not contract or contract["_id"] != workspan["contract"]:
-                contract = udb.get_user_active_contract(self.database.users, self.user_id, workspan["from"])
+            if workspan["contract"]:
+                if not contract:
+                    contract = udb.get_user_contract_by_id(self.database, self.user_id, workspan["contract"])
 
-            if not contract:
-                raise ValueError("Pro některé zapsané hodiny uživatele nepřipadá platná smlouva.")
+            else:
+                print(f"Pro tuto práci neexistuje platná smlouva: {workspan}")
+                continue  # TODO vyřešit hlášení do frontendu
 
             hours_worked += workspan["hours"]
-            gross_wage += workspan["hours"] * contract["hour_rate"]
+
+        if not hours_worked or not contract:
+            return 0, 0, 0
+
+        gross_wage = contract["hour_rate"] * hours_worked
 
         tax_amount = calculate_tax(gross_wage, self.tax_rate,
                                    self.tax_deduction if has_tax_declaration else 0,
@@ -117,7 +130,7 @@ class AttendanceCalculator:
         gross_wage = 0
         tax_amount = 0
         for month in range(12):
-            start_of_month = datetime(self.year.year, month, 1)
+            start_of_month = datetime(self.year.year, month + 1, 1)
             month_hours_worked, month_gross_wage, month_tax_amount = self._calculate_given_month(start_of_month)
 
             hours_worked += month_hours_worked
@@ -127,6 +140,14 @@ class AttendanceCalculator:
         self._year_hours_worked = hours_worked
         self._year_gross_wage = gross_wage
         self._year_tax_amount = tax_amount
+
+    def _month_has_study_certificate(self, month):
+        return bool(udb.get_user_active_documents(self.database, self.user_id, "study_certificate",
+                                                  month, month + relativedelta(months=1)))
+
+    def _month_has_tax_declaration(self, month):
+        return bool(udb.get_user_active_documents(self.database, self.user_id, "tax_declaration",
+                                                  month, month + relativedelta(months=1)))
 
     @property
     def month_hours_worked(self):
@@ -152,80 +173,81 @@ class AttendanceCalculator:
     @property
     def year_hours_worked(self):
         if self._year_hours_worked is None:
-            self._calculate_this_month()
+            self._calculate_this_year()
 
         return self._year_hours_worked
 
     @property
     def year_gross_wage(self):
         if self._year_gross_wage is None:
-            self._calculate_this_month()
+            self._calculate_this_year()
 
         return self._year_gross_wage
 
     @property
     def year_tax_amount(self):
         if self._year_tax_amount is None:
-            self._calculate_this_month()
+            self._calculate_this_year()
 
         return self._year_tax_amount
 
     @property
     def month_hour_rate(self):
-        return self.month_gross_wage / self.month_hours_worked
+        if self._month_hour_rate is None:
+            self._calculate_this_month()
+
+        month_contracts = udb.get_user_active_contracts(self.database, self.user_id, self.month,
+                                                        self.month + relativedelta(months=1))
+        if month_contracts:
+            return month_contracts[0]["hour_rate"]
+        else:
+            return None
 
     @property
-    def year_hour_rate(self):
-        return self.year_gross_wage / self.year_hours_worked
-
     def month_net_wage(self):
         return self.month_gross_wage - self.month_tax_amount
 
+    @property
     def year_net_wage(self):
         return self.year_gross_wage - self.year_tax_amount
 
+    @property
+    def month_max_hours(self):
+        if self.month_hour_rate == 0:
+            return None
+        return floor_to_half(self.month_max_gross_wage / self.month_hour_rate)
+
+    @property
     def month_available_hours(self):
-        return
+        return self.month_max_hours - self.month_hours_worked
+
+    @property
+    def year_available_hours(self):
+        return self.year_max_hours - self.year_hours_worked
 
 
 class ApiAdminMonthTableHandler(BaseHandler):
 
     def get(self, date):
-        month = str_ops.datetime_from_iso_str(date).replace(day=1)
-        next_month = month + relativedelta(months=1)
-
+        date = str_ops.datetime_from_iso_str(date)
         rows = []
 
         users = udb.get_users(self.mdb.users)
         for user in users:
-            # TODO tady je edge case: smlouva a dokumenty s kontrolují k prvnímu dnu v měsíci
-            # a je předpoklad, že platí celý měsíc!
-            active_contract = udb.get_user_active_contract(self.mdb.users, user["_id"], month)
-            apply_deduction = bool(udb.get_user_active_tax_declaration(self.mdb.users, user["_id"], month))
-            apply_deduction_student = bool(udb.get_user_active_study_certificate(self.mdb.users, user["_id"], month))
-
-            hour_rate = active_contract["hour_rate"] if active_contract else 0
-            month_workspans = adb.get_user_workspans(self.mdb, user["_id"], month, next_month)
-            hours_worked = sum(ws["hours"] for ws in month_workspans)
-            gross_wage = hours_worked * hour_rate
-            tax_amount = calculate_tax(gross_wage,
-                                       self.dpp_params["tax_rate"],
-                                       self.dpp_params["tax_deduction"] if apply_deduction else 0,
-                                       self.dpp_params["tax_deduction_student"] if apply_deduction_student else 0)
-            net_wage = gross_wage - tax_amount
+            calculator = AttendanceCalculator(self.mdb, user["_id"], date)
 
             row = {
                 "id": user["_id"],
                 "name": user.get("name", {}),
-                "hours_worked": hours_worked,
-                "hour_rate": hour_rate,
+                "hours_worked": calculator.month_hours_worked,
+                "hour_rate": calculator.month_hour_rate,
                 "month_closed": user.get("month_closed", False),  # TODO tak jak nyní month_closed funguje nedává smysl,
                 # v db je reprezentován bool hodnotou, ale to znamená že ta bude skákat každý měsíc. Větší smysl dává
                 # mít pole months_closed or closed_months a do něj přidávat uzavřené měsíce (třeba data prvních dnů
                 # v měsíci v iso tvaru.
-                "gross_wage": gross_wage,
-                "tax_amount": tax_amount,
-                "net_wage": net_wage,
+                "gross_wage": calculator.month_gross_wage,
+                "tax_amount": calculator.month_tax_amount,
+                "net_wage": calculator.month_net_wage,
             }
             rows.append(row)
 
@@ -235,29 +257,21 @@ class ApiAdminMonthTableHandler(BaseHandler):
 class ApiAdminYearTableHandler(BaseHandler):
 
     def get(self, date):
-        year = str_ops.datetime_from_iso_str(date).replace(day=1, month=1)
-        next_year = year + relativedelta(years=1)
+        date = str_ops.datetime_from_iso_str(date)
 
         rows = []
 
         users = udb.get_users(self.mdb.users)
         for user in users:
-            active_contract = udb.get_user_active_contract(self.mdb.users, user["_id"])
-            hour_rate = active_contract["hour_rate"] if active_contract else 0
-            year_workspans = adb.get_user_workspans(self.mdb, user["_id"], year, next_year)
-            hours_worked = sum(ws["hours"] for ws in year_workspans)
-            gross_wage = hours_worked * hour_rate
-            tax_amount = 0
-            net_wage = gross_wage - tax_amount
+            calculator = AttendanceCalculator(self.mdb, user["_id"], date)
 
             row = {
                 "id": user["_id"],
                 "name": user.get("name", {}),
-                "hours_worked": hours_worked,
-                "hour_rate": hour_rate,
-                "gross_wage": gross_wage,
-                "tax_amount": tax_amount,
-                "net_wage": net_wage,
+                "hours_worked": calculator.year_hours_worked,
+                "gross_wage": calculator.year_gross_wage,
+                "tax_amount": calculator.year_tax_amount,
+                "net_wage": calculator.year_net_wage,
             }
             rows.append(row)
 
@@ -295,9 +309,7 @@ class UserAttendanceHandler(BaseHandler):
             "workspans": day_workspans,
             "vacations": current_and_future_vacations,
             "is_vacation_day": is_vacation_day,
-            # "year_days_of_vacation": get_user_year_days_of_vacation(self.mdb.users, user_id, date)
         }
-        # template_params.update(compile_user_month_info(self.mdb.users, user_id, datetime.now()))
 
         self.render("attendance.home.hbs", **template_params)
 
@@ -305,6 +317,7 @@ class UserAttendanceHandler(BaseHandler):
 class ApiCalendarHandler(BaseHandler):
 
     def post(self, user_id, date):
+        # TODO je potřeba v kalendáři mít přístup i k assignmentům
         month = str_ops.datetime_from_iso_str(date).replace(day=1)
         num_days_in_month = calendar.monthrange(month.year, month.month)[1]
 
@@ -345,48 +358,37 @@ class ApiMonthInfoHandler(BaseHandler):
     def post(self, user_id, date):
         month = str_ops.datetime_from_iso_str(date).replace(day=1)
 
-        data = compile_user_month_info(self.mdb, user_id, month,
-                                       self.dpp_params["year_max_hours"],
-                                       self.dpp_params["month_max_gross_wage"])
-        data["year_days_of_vacation"] = get_user_days_of_vacation_in_year(self.mdb, user_id, month)
+        calculator = AttendanceCalculator(self.mdb, user_id, month)
+
+        data = {
+            "month_hours_worked": calculator.month_hours_worked,
+            "year_hours_worked": calculator.year_hours_worked,
+            "hour_rate": calculator.month_hour_rate,
+            "year_max_hours": calculator.year_max_hours,
+            "month_max_hours": calculator.month_max_hours,
+            "month_available_hours": calculator.month_available_hours,
+            "year_available_hours": calculator.year_available_hours,
+            "month_gross_wage": calculator.month_gross_wage,
+            "month_tax_amount": calculator.month_tax_amount,
+            "month_net_wage": calculator.month_net_wage,
+            "year_days_of_vacation": get_user_days_of_vacation_in_year(self.mdb, user_id, month),
+        }
 
         self.write(bson.json_util.dumps(data))
 
 
-class ApiAddWorkspanHandler(BaseHandler):
-
-    def post(self, user_id):
-        req = self.request.body.decode("utf-8")
-        form_data = bson.json_util.loads(req)
-
-        today = str_ops.datetime_from_iso_str(form_data["date"])
-
-        workspan = {}
-        workspan["from"] = str_ops.datetime_from_iso_string_and_time_string(form_data["date"], form_data["from"])
-        workspan["hours"] = float(form_data["hours"])
-        workspan["assignment"] = form_data["assignment"]
-
-        for doctype in ["study_certificate", "tax_declaration"]:
-            document_mdoc = udb.get_user_active_document(self.mdb.users, user_id, doctype, today)
-            workspan[doctype] = document_mdoc["_id"] if document_mdoc else None
-
-        contract_mdoc = udb.get_user_active_contract(self.mdb.users, user_id, today)
-        workspan["contract"] = contract_mdoc["_id"] if contract_mdoc else None
-
-        self.check_vacations_conflicts(user_id, workspan)
-        self.check_workspans_conflicts(user_id, workspan)
-
-        print("ukladam workspan", workspan)
-
-        adb.add_user_workspan(self.mdb, user_id, workspan)
+class WorkspanBaseHandler(BaseHandler):
 
     def check_vacations_conflicts(self, user_id, workspan):
+        """ returns true if there is no conflict """
         # z databáze dostaneme dovolené končící dnes nebo později seřazené podle data konce.
         # první dovolená je nejblíž, stačí tedy zkontrolovat, jestli začíná dnes nebo dříve.
         today = workspan["from"].replace(hour=0, minute=0, second=0, microsecond=0)
         vacations = adb.get_user_vacations(self.mdb, user_id, today)
         if vacations and vacations[0]["from"] <= workspan["from"]:
-            raise BadInputError("Na dovolené se nepracuje.")
+            return False
+
+        return True
 
     def check_workspans_conflicts(self, user_id, workspan):
         today = workspan["from"].replace(hour=0, minute=0, second=0, microsecond=0)
@@ -400,7 +402,63 @@ class ApiAddWorkspanHandler(BaseHandler):
             earliest_end = min(workspan_end, other_ws["from"] + relativedelta(minutes=int(other_ws["hours"]) * 60))
 
             if latest_start < earliest_end:
-                raise BadInputError("Časový konflikt s jinou prací.")
+                return False
+
+        return True
+
+
+class ApiAddWorkspanHandler(WorkspanBaseHandler):
+
+    def post(self, user_id):
+        req = self.request.body.decode("utf-8")
+        form_data = bson.json_util.loads(req)
+
+        today = str_ops.datetime_from_iso_str(form_data["date"])
+
+        workspan = {}
+        workspan["from"] = str_ops.datetime_from_iso_string_and_time_string(form_data["date"], form_data["from"])
+        workspan["hours"] = float(form_data["hours"])
+        workspan["assignment"] = form_data["assignment"]
+
+        contract_mdoc = udb.get_user_active_contract(self.mdb.users, user_id, today)
+        workspan["contract"] = contract_mdoc["_id"] if contract_mdoc else None
+
+        if not self.check_vacations_conflicts(user_id, workspan):
+            raise BadInputError("Na dovolené se nepracuje.")
+
+        if not self.check_workspans_conflicts(user_id, workspan):
+            raise BadInputError("Časový konflikt s jinou prací.")
+
+        print("ukladam workspan", workspan)
+
+        adb.add_user_workspan(self.mdb, user_id, workspan)
+
+
+class ApiMonthWorkspansHandler(BaseHandler):
+
+    def post(self, user_id):
+        req = self.request.body.decode("utf-8")
+        form_data = bson.json_util.loads(req)
+
+        for day_dict in form_data:
+            today = str_ops.datetime_from_iso_str(day_dict["date"])
+            tomorow = today + relativedelta(days=1)
+
+            workspan = {}
+            workspan["from"] = today
+            workspan["hours"] = float(day_dict["hours"])
+            workspan["assignment"] = day_dict["assignment"]
+
+            contract_mdoc = udb.get_user_active_contract(self.mdb.users, user_id, today)
+            workspan["contract"] = contract_mdoc["_id"] if contract_mdoc else None
+
+            todays_workspans = adb.get_user_workspans(self.mdb, user_id, today, tomorow)
+
+            for today_workspan in todays_workspans:
+                adb.delete_user_workspan(self.mdb, user_id, today_workspan["_id"])
+
+            if workspan["hours"] > 0:
+                adb.add_user_workspan(self.mdb, user_id, workspan)
 
 
 class ApiAddVacationHandler(BaseHandler):
